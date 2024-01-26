4aeb78ba4667950a96eafaf195e60b0f0d408ab3
Vyshnavi Kurella <118326223+VyshnaviKurella@users.noreply.github.com>
2024-01-25 18:49:28 -0800

    Runtime: 0 ms (faster than 100.0%), Memory: 40.9 MB (less than 7.15%) - GitSync

---
+++ 509-fibonacci-number/509. Fibonacci Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        int a=0,b=1,sum=0;
+        if(n==1)
+        {
+            return 1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            sum =a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
+}
